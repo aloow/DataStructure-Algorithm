cc3eea401cb193c44cd43792ec774f6f47a70e8d
Shao <897034809@qq.com>
2019-12-02 10:23:03 +0800

    添加Python集合类使用

---
+++ python/Python.py
@@ -0,0 +1,61 @@
+# encoding: utf-8
+import os
+from collections import deque
+
+# 字典
+
+# 字符串
+    
+    
+def main():
+ 
+ 
+ 
+
+if __name__ == "__main__":
+    main()
+
+""" 集合类型Collection
+长度：
+length = len(collect)
+"""
+# ----------------------
+
+
+""" 字典的使用
+1.初始化:
+dict = {}
+
+2.取值,如果键不存在，返回默认值。不会设置默认值到key中
+dict.get(key, default=None)
+使用：dict('name') dict('name', 10)
+
+3.存在判断
+是否在：if i in dict:    是否不在：if i not in dict:
+
+4.返回所有
+dict.keys() dict.values()
+
+5.设置值
+dict[key] = valut
+
+6.清空
+dict.clear()
+
+字符变字符串  '2' ->  str('2')
+"""
+# ----------------------
+
+""" 字符串
+遍历：
+for i in s:
+    print(i)  # 其中i还是字符串
+
+另外一种遍历，是遍历字符
+for i in range(len(s)):
+    print s[i] # 其中s[i]是字符
+"""
+# ----------------------
+
+"""
+"""
